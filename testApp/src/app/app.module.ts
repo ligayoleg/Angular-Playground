1b54313ea982e20f16f96f767c55c3f5d559d162
Oleg <ligayoleg@gmail.com>
2023-07-19 08:26:12 -0400

    added routes, parent and child components

--- testApp/src/app/app.module.ts
+++ testApp/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
+import { ParentComponent } from './sharing-data/components/parent/parent.component';
+import { ChildComponent } from './sharing-data/components/child/child.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
