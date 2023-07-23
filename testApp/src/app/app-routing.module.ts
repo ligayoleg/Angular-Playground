ecf59534009f0c75290dcfba1606a8ad0952cc24
Oleg <ligayoleg@gmail.com>
2023-07-22 23:25:51 -0400

    Added Home page and nav
    Added home page and navigation module with dropdown option provided by bootstrap

--- testApp/src/app/app-routing.module.ts
+++ testApp/src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ParentComponent } from './sharing-data/components/parent/parent.component';
 import { OfComponent } from './rxjs/components/of/of.component';
 import { OperatorsComponent } from './rxjs/components/operators/operators.component';
+import { HomeComponent } from './layout/home/components/home.component';
+
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'parent', component: ParentComponent},
   {path: 'of', component: OfComponent},
   {path: 'operators', component: OperatorsComponent}
--- testApp/src/app/app.module.ts
+++ testApp/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 //Components
 import { AppComponent } from './app.component';
+
 import { ParentComponent } from './sharing-data/components/parent/parent.component';
 import { ChildComponent } from './sharing-data/components/child/child.component';
 import { OperatorsComponent } from './rxjs/components/operators/operators.component';
 import { OfComponent } from './rxjs/components/of/of.component';
 import { TestComponent } from './test/test.component';
+import { NavComponent } from './layout/nav/components/nav.component';
+import { HomeComponent } from './layout/home/components/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
+    HomeComponent,
     ParentComponent,
     ChildComponent,
     OfComponent,
